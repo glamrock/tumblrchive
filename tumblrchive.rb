2601358d5036d87c96823b59f88c7f1a3104d3be
Daniel Assange <somnidea@lemma.org>
2010-09-03 04:03:40 +1000

    Use API and don't full-archive unless asked, optional email alerts.

--- tumblrchive.rb
+++ tumblrchive.rb
@@ -1,42 +1,122 @@
 #!/usr/bin/env ruby
 
 require 'rubygems'
+require 'optparse'
 require 'hpricot'
 require 'open-uri'
+require 'rexml/document'
+require 'pony'
 
 class Tumblrchiver
-	def initialize(baseurl, odir=nil)
+
+	# @param baseurl Front page of the tumblr.
+	# @param options Options hash (:full, :output_dir).
+	def initialize(baseurl, options)
 		@baseurl = baseurl
 		@baseurl = "http://" + @baseurl unless @baseurl.include?("http://")
 		@baseurl += '/' unless @baseurl[-1] == '/'
-		@odir = odir || @baseurl.scan(/\/([^\/]+?)\//)[0][0]
+
+		@domain = @baseurl.scan(/\/([^\/]+?)\//)[0][0]
+
+		@options = options
+		@odir = options[:output_dir] || @domain
 	end
 
 	def start
 		system "mkdir #{@odir}"
-		1.upto(1.0/0.0) do |i|
-			url = @baseurl + "page/#{i}"		
 
-			path = File.join(@odir, "#{i}.html")
+		offset = 0
 
-			system "wget -O #{path} #{url}"
 
-			doc = Hpricot(File.read(path))
+		while true
+			xml = open(@baseurl + "api/read?start=#{offset}").read
+			doc = REXML::Document.new(xml)
 
-			if doc.search('div.post').length == 0
-				File.delete(path)
-				break
+			foundone = false
+			doc.each_element('//post') do |post|
+				path = File.join(@odir, "#{post.attributes['id']}.html")
+
+				if !File.exists?(path) || @options[:full]
+					foundone = true
+
+
+
+					if @options[:email_alert]
+						title = post.get_elements("regular-title")[0].text
+						body = post.get_elements("regular-body")[0].text
+						
+
+						emh = @options[:email_header] || ""
+						emf = @options[:email_footer] || ""
+
+						Pony.mail(:to => @options[:email_to],
+											:from => @options[:email_from],
+											:subject => "New '#{@domain}' post: '#{title}'",
+											:html_body => "#{emh}\n\n#{body}\n\n#{emf}",
+											:via => :smtp)
+					end
+
+					system "wget -O #{path} #{post.attributes['url']}"
+				end
 			end
+
+			break unless foundone
+
+			offset += 20
 		end
 	end
 end
 
 if __FILE__ == $0
-	if ARGV.length < 1
-		puts "Usage: tumblrchive target_url [output_dir]"
-		exit
+	options = {}
+
+	optparse = OptionParser.new do |opts|
+		opts.banner = "Usage: tumblrchive.rb [options] baseurl [output_dir]"
+
+		opts.on('-f', '--full', "Full archive; overwrite existing files.") do
+			options[:full] = true	
+		end
+
+		opts.on('-o DIR', '--output-dir DIR', 'Output directory') do |dir|
+			options[:output_dir] = dir
+		end
+
+		opts.on('-e', '--email-alert', 'Send email alert if new post is detected') do
+			options[:email_alert] = true
+		end
+
+		opts.on('--email-from FROM', 'On email alert, send from this address') do |from|
+			options[:email_from] = from
+		end
+
+		opts.on('--email-to TO', 'On email alert, send to this address') do |to|
+			options[:email_to] = to
+		end
+
+		opts.on('--email-header HEADER', 'A header to be added to the email alert') do |header|
+			options[:email_header] = header
+		end
+
+		opts.on('--email-footer FOOTER', 'A footer to be added to the email alert') do |footer|
+			options[:email_footer] = footer
+		end
+
+		opts.on('-h', '--help', "Display this help message") do
+			puts opts
+			exit
+		end
+
+		opts.parse!(ARGV)
+
+		p ARGV
+		if ARGV.length < 1
+			puts opts
+			exit
+		end
 	end
 
-	archiver = Tumblrchiver.new(ARGV[0], ARGV[1])
+	p options
+
+	archiver = Tumblrchiver.new(ARGV[0], options)
 	archiver.start
 end
